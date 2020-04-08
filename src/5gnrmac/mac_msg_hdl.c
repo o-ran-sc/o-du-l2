e550b2091fb29289d12a5722b189acbede7862f0
Balaji Shankaran <balaji.shankaran@radisys.com>
2020-04-08 15:06:35 +0530

    SIB1 optional parameters, Cell start request, stop request and slot indication
    PatchSet 5:
    Modified :
     1. mac_msg_hdl.c
     2. rg.h
     3. du_mgr.h
     4. du_msg_hdl.c
    
    PatchSet 4:
    deleted :
     1. mac_main.h
    modified :
     1. mac_msg_hdl.c
     2. mac_upr_int_api.h
     3. rg.h
     4. du_mgr.h
     5. du_msg_hdl.c
    
    Change-Id: I857386e4d028f1ee85711eaa650978f4e2d85a55
    Signed-off-by: Balaji Shankaran <balaji.shankaran@radisys.com>

---
+++ src/5gnrmac/mac_msg_hdl.c
@@ -0,0 +1,121 @@
+/*******************************************************************************
+################################################################################
+#   Copyright (c) [2017-2019] [Radisys]                                        #
+#                                                                              #
+#   Licensed under the Apache License, Version 2.0 (the "License");            #
+#   you may not use this file except in compliance with the License.           #
+#   You may obtain a copy of the License at                                    #
+#                                                                              #
+#       http://www.apache.org/licenses/LICENSE-2.0                             #
+#                                                                              #
+#   Unless required by applicable law or agreed to in writing, software        #
+#   distributed under the License is distributed on an "AS IS" BASIS,          #
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+#   See the License for the specific language governing permissions and        #
+#   limitations under the License.                                             #
+################################################################################
+*******************************************************************************/
+
+/* header include files -- defines (.h)  */
+#include "envopt.h"        /* environment options */
+#include "envdep.h"        /* environment dependent */
+#include "envind.h"        /* environment independent */
+#include "gen.h"           /* general layer */
+#include "ssi.h"           /* system services */
+#include "cm5.h"           /* common timers defines */
+#include "cm_hash.h"       /* common hash list defines */
+#include "cm_llist.h"      /* common linked list defines */
+#include "cm_mblk.h"       /* memory management */
+#include "cm_tkns.h"       /* common tokens */
+#include "cm_lte.h"        /* common tokens */
+#include "rgu.h"
+#include "tfu.h"
+#include "rg_sch_inf.h"
+#include "rg_env.h"
+#include "lrg.h"
+#include "crg.h"
+#include "rg.h"
+#include "du_log.h"
+
+/* header/extern include files (.x) */
+#include "gen.x"           /* general layer typedefs */
+#include "ssi.x"           /* system services typedefs */
+#include "cm5.x"           /* common timers */
+#include "cm_hash.x"       /* common hash list */
+#include "cm_lib.x"        /* common library */
+#include "cm_llist.x"      /* common linked list */
+#include "cm_mblk.x"       /* memory management */
+#include "cm_tkns.x"       /* common tokens */
+#include "cm_lte.x"        /* common tokens */
+#include "rgu.x"
+#include "tfu.x"
+#include "rg_sch_inf.x"
+#include "lrg.x"
+#include "crg.x"
+#include "rg_prg.x"
+#include "du_app_mac_inf.h"
+#include "rg.x"
+#include "fapi_interface.h"
+
+/* This file contains message handling functionality for MAC */
+
+/*******************************************************************
+ *
+ * @brief Handles cell start reuqest from DU APP
+ *
+ * @details
+ *
+ *    Function : MacHdlCellStartReq
+ *
+ *    Functionality:
+ *      Handles cell start reuqest from DU APP
+ *
+ * @params[in] Post structure pointer
+ *             Cell Start Request info pointer 
+ * @return ROK     - success
+ *         RFAILED - failure
+ *
+ * ****************************************************************/
+uint16_t MacHdlCellStartReq(Pst *pst, MacCellStartInfo  *cellStartInfo)
+{
+   DU_LOG("\nMAC : Handling cell start request");
+   //sendToLowerMac(START_REQ, 0, cellStartInfo);
+
+   MAC_FREE_MEM(pst->region, pst->pool, cellStartInfo, \
+	   sizeof(MacCellStartInfo));
+
+   return ROK;
+}
+
+/*******************************************************************
+ *
+ * @brief Handles cell stop request from DU APP
+ *
+ * @details
+ *
+ *    Function : MacHdlCellStartReq
+ *
+ *    Functionality:
+ *        Handles cell stop request from DU APP
+ *
+ * @params[in] Post structure pointer
+ *             Mac Cell stop information
+ * @return ROK     - success
+ *         RFAILED - failure
+ *
+ * ****************************************************************/
+uint16_t MacHdlCellStopReq(Pst *pst, MacCellStopInfo  *cellStopInfo)
+{
+   DU_LOG("\nMAC : Handling cell start request");
+   //sendToLowerMac(STOP_REQ, 0, cellStopInfo);
+
+   MAC_FREE_MEM(pst->region, pst->pool, cellStopInfo, \
+	   sizeof(MacCellStopInfo));
+ 
+   return ROK;
+}
+
+/**********************************************************************
+         End of file
+**********************************************************************/
+
--- src/5gnrmac/mac_upr_inf_api.h
+++ src/5gnrmac/mac_upr_inf_api.h
@@ -16,26 +16,38 @@
 ################################################################################
 *******************************************************************************/
 
-#include "odu_common_codec.h"
+/* This file contains the definitions for Upper Interface APIs that are
+ * invoked from MAC */
 
-/* Macros */
-#define ODU_VALUE_ZERO 0
-#define ODU_VALUE_ONE 1
-#define ODU_VALUE_TWO 2
-#define ODU_VALUE_THREE 3
-#define ODU_VALUE_FOUR 4
-#define ODU_VALUE_FIVE 5
-#define ODU_VALUE_SEVEN 7
-#define ODU_VALUE_EIGHT 8
-#define ODU_VALUE_NINE  9 
-#define ODU_VALUE_TEN  10
+/* header include files -- defines (.h)  */
+#include "envopt.h"        /* environment options */
+#include "envdep.h"        /* environment dependent */
+#include "envind.h"        /* environment independent */
+#include "gen.h"           /* general layer */
+#include "ssi.h"           /* system services */
+#include "cm5.h"           /* common timers defines */
+#include "cm_hash.h"       /* common hash list defines */
+#include "cm_llist.h"      /* common linked list defines */
+#include "cm_mblk.h"       /* memory management */
+#include "cm_tkns.h"       /* common tokens */
+#include "cm_lte.h"        /* common tokens */
+
+/* header/extern include files (.x) */
+#include "gen.x"           /* general layer typedefs */
+#include "ssi.x"           /* system services typedefs */
+#include "cm5.x"           /* common timers */
+#include "cm_hash.x"       /* common hash list */
+#include "cm_lib.x"        /* common library */
+#include "cm_llist.x"      /* common linked list */
+#include "cm_mblk.x"       /* memory management */
+#include "cm_tkns.x"       /* common tokens */
+#include "cm_lte.x"        /* common tokens */
+#include "du_app_mac_inf.h"
 
 
-/* Function declarations */
-int BuildMibPdu();
-int BuildMibMsg();
-int BuildSib1Msg();
+uint16_t MacDuAppSlotInd(Pst *pst, SlotInfo *slotInfo);
 
 /**********************************************************************
          End of file
 **********************************************************************/
+
