8f771800ea102d16e0bf59f81327f573ee3afdcd
Balaji Shankaran <balaji.shankaran@radisys.com>
2020-05-15 09:42:41 +0530

    E2setupRequest Clean up changes
    Change-Id: I13ea3451cf6b2e8a9ee8d7d1d8bd1adf4b0e8e6d
    Signed-off-by: Balaji Shankaran <balaji.shankaran@radisys.com>

--- src/du_app/du_e2ap_msg_hdl.h
+++ src/du_app/du_e2ap_msg_hdl.h
@@ -62,6 +62,8 @@ typedef struct e2apDb
 S16 BuildAndSendE2SetupReq();
 S16 SendE2APMsg(Region , Pool );
 void E2APMsgHdlr(Buffer *mBuf);
+uint16_t deAllocateE2SetupReqMsg(E2AP_PDU_t *e2apMsg, \
+  E2setupRequest_t *e2SetupReq, uint16_t idx);
 
 /**********************************************************************
          End of file
