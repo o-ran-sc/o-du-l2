70bf0719b4f70196f96bc1e5391b73c0795a0aef
balajihands <balaji.shankaran@radisys.com>
2020-05-27 09:55:14 +0530

    forming mac pdu
    Change-Id: I06ecaf3081e2187b9fb5fd0526845859adb7faa8
    Signed-off-by: Balaji Shankaran <balaji.shankaran@radisys.com>

--- src/5gnrmac/mac_msg_hdl.c
+++ src/5gnrmac/mac_msg_hdl.c
@@ -62,6 +62,7 @@
 /* This file contains message handling functionality for MAC */
 
 extern void sendToLowerMac(uint16_t msgType, uint32_t msgLen, void *msg);
+uint16_t buildMacPdu(RlcMacData *dlData);
 
 /* Function pointer for sending crc ind from MAC to SCH */
 MacSchCrcIndFunc macSchCrcIndOpts[]=
