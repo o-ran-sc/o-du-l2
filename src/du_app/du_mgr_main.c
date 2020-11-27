37badc1a711fc7802d2f0b67924f3c15e569a7d5
lal.harshita <harshita.lal@radisys.com>
2020-11-27 18:53:31 +0530

    Developer-guide.rst
    Change-Id: Ic58e014a32e475d2297527c155f4ec609faa8a05
    Signed-off-by: lal.harshita <harshita.lal@radisys.com>

--- src/du_app/du_mgr_main.c
+++ src/du_app/du_mgr_main.c
@@ -291,7 +291,7 @@ uint8_t commonInit()
    ODU_SET_PROC_ID(DU_PROC);
 
    /* system task for DU APP */
-   if(SCreateSTsk(PRIOR0, &du_app_stsk) != ROK)
+   if(ODU_CREATE_TASK(PRIOR0, &du_app_stsk) != ROK)
    {
       DU_LOG("\nDU_APP : System Task creation for DU APP failed");
       return RFAILED;
@@ -299,7 +299,7 @@ uint8_t commonInit()
    ODU_SET_THREAD_AFFINITY(&du_app_stsk, SS_AFFINITY_MODE_EXCL, 15, 0);
 
    /* system task for RLC_DL and MAC */
-   if(SCreateSTsk(PRIOR0, &rlc_mac_cl_stsk) != ROK)
+   if(ODU_CREATE_TASK(PRIOR0, &rlc_mac_cl_stsk) != ROK)
    {
       DU_LOG("\nDU_APP : System Task creation for RLC DL/MAC failed");
       return RFAILED;
@@ -309,7 +309,7 @@ uint8_t commonInit()
    ODU_SET_THREAD_AFFINITY(&rlc_mac_cl_stsk, SS_AFFINITY_MODE_EXCL, 16, 0);
 
    /* system task for RLC UL */
-   if(SCreateSTsk(PRIOR1, &rlc_ul_stsk) != ROK)
+   if(ODU_CREATE_TASK(PRIOR1, &rlc_ul_stsk) != ROK)
    {
       DU_LOG("\nDU_APP : System Task creation for RLC UL failed");
       return RFAILED;
@@ -317,7 +317,7 @@ uint8_t commonInit()
    ODU_SET_THREAD_AFFINITY(&rlc_ul_stsk, SS_AFFINITY_MODE_EXCL, 17, 0);
 
    /* system task for SCTP receiver thread */
-   if(SCreateSTsk(PRIOR0, &sctp_stsk) != ROK)
+   if(ODU_CREATE_TASK(PRIOR0, &sctp_stsk) != ROK)
    {
       DU_LOG("\nDU_APP : System Task creation for SCTP failed");
       return RFAILED;
@@ -325,7 +325,7 @@ uint8_t commonInit()
    ODU_SET_THREAD_AFFINITY(&sctp_stsk, SS_AFFINITY_MODE_EXCL, 18, 0);
 
    /* system task for lower-mac receiver thread */
-   if(SCreateSTsk(PRIOR0, &lwr_mac_stsk) != ROK)
+   if(ODU_CREATE_TASK(PRIOR0, &lwr_mac_stsk) != ROK)
    {
       DU_LOG("\nDU_APP : System Task creation for Lower MAC failed");
       return RFAILED;
