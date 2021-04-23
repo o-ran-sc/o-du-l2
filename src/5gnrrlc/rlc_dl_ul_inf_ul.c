0dd81a25ab85a8dd2ab7519e5e21c06681de448c
sphoorthi <sphoorthi.dayanand@radisys.com>
2021-04-23 20:06:33 +0530

    [JIRA ID:ODUHIGH-331]-Renaming of RLC files
    Change-Id: I909e86987ca63f64aaa2fb353c28f1d21a564384
    Signed-off-by: sphoorthi <sphoorthi.dayanand@radisys.com>

---
+++ src/5gnrrlc/rlc_dl_ul_inf_ul.c
@@ -0,0 +1,572 @@
+/*******************************************************************************
+################################################################################
+#   Copyright (c) [2017-2019] [Radisys]                                        #
+#                                                                              #
+#   Licensed under the Apache License, Version 2.0 (the "License");            #
+#   you may not use this file except in compliance with the License.           #
+#   You may obtain a copy of the License at                                    #
+#                                                                              #
+#       http://www.apache.org/licenses/LICENSE-2.0                             #
+#                                                                              #
+#   Unless required by applicable law or agreed to in writing, software        #
+#   distributed under the License is distributed on an "AS IS" BASIS,          #
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+#   See the License for the specific language governing permissions and        #
+#   limitations under the License.                                             #
+################################################################################
+*******************************************************************************/
+
+/**********************************************************************
+ 
+    Name:   NR RLC layer
+ 
+    Type:   C include file
+ 
+    Desc:   Defines required by LTE MAC
+ 
+    File:   rlc_dl_ul_inf_ul.c
+ 
+**********************************************************************/
+
+/** @file rlc_dl_ul_inf_ul.c
+@brief  UDX Uplink Module 
+*/
+ 
+/* header include files (.h) */
+#include "common_def.h"
+#include "lkw.h"           /* LKW defines */
+#include "ckw.h"           /* CKW defines */
+#include "kwu.h"           /* KWU defines */
+#include "rgu.h"           /* RGU defines */
+#include "rlc_err.h"        /* Err defines */
+#include "rlc_env.h"        /* RLC environment options */
+
+#include "rlc_utils.h"            /* RLC defines */
+#include "rlc_dl_ul_inf.h"
+#include "rlc_ul.h"
+#include "rlc_dl.h"
+
+/* extern (.x) include files */
+#include "lkw.x"           /* LKW */
+#include "ckw.x"           /* CKW */
+#include "kwu.x"           /* KWU */
+#include "rgu.x"           /* RGU */
+
+#include "rlc_utils.x"
+#include "rlc_dl_ul_inf.x"
+#include "rlc_dl.x"
+#include "rlc_ul.x"
+
+#include "du_app_rlc_inf.h"
+#include "rlc_mgr.h"
+
+#define RLC_MODULE RLC_DBGMASK_UDX
+/* local defines */
+
+/* local externs */
+
+/* forward references */
+
+/* public variable declarations */
+S16 rlcHdlCrlcUlCfgReq ARGS((RlcCb  *gCb,RlcUlCfgTmpData *cfgTmpData,
+                                  RlcCfgCfmInfo *cfmInfo, RlcCfgCfmInfo *cfgCfm));
+
+/**
+  * @brief
+  * UDX APIs
+  */
+/**
+ * @brief 
+ *    Handler for bind confirmation from DL.
+ *
+ * @details
+ *    This function handles the bind confirmation received
+ *    from DL. 
+ *
+ * @param[in] pst     Post structure  
+ * @param[in] suId    Service User ID
+ * @param[in] status  Status of confirmation 
+ * 
+ * @return  S16
+ *    -# ROK 
+ *    -# RFAILED
+ */
+
+S16 rlcUlUdxBndCfm
+(
+Pst    *pst,  
+SuId   suId, 
+uint8_t status 
+)
+{
+   uint16_t        event;        
+   uint16_t        cause;       
+   RlcUdxUlSapCb   *udxSap;   /* RGU SAP Control Block */
+   RlcCb           *tRlcCb;
+
+#if (ERRCLASS & ERRCLS_INT_PAR)
+   if (pst->dstInst >= MAX_RLC_INSTANCES)
+   {
+      return  (RFAILED);
+   }
+#endif
+   tRlcCb = RLC_GET_RLCCB(pst->dstInst);
+
+   RLCDBGP_BRIEF(tRlcCb, "rlcUlUdxBndCfm(post, suId(%d), status(%d)\n", 
+                suId, status);
+
+#if (ERRCLASS & ERRCLS_INT_PAR)
+   if (tRlcCb->init.cfgDone != TRUE)
+   {
+      DU_LOG("\nERROR  -->  RLC_UL : General configuration not done");
+      RLC_SEND_SAPID_ALARM(tRlcCb,suId, 
+                          LKW_EVENT_LI_BND_CFM, LCM_CAUSE_INV_STATE);
+
+      return RFAILED;
+   }
+
+   if (suId < 0)
+   {
+      DU_LOG("\nERROR  -->  RLC_UL : Invalid suId");
+      RLC_SEND_SAPID_ALARM(tRlcCb,suId, 
+                           LKW_EVENT_LI_BND_CFM, LCM_CAUSE_INV_SUID);
+
+      return RFAILED;
+   }
+#endif /* ERRCLASS & ERRCLS_INT_PAR */
+
+   udxSap = tRlcCb->u.ulCb->udxUlSap + suId;
+
+   RLCDBGP_DETAIL(tRlcCb, "RlcLiRguBndCfm: For RGU SAP state=%d\n", 
+                 udxSap->state);
+
+   /* Check rguSap state */
+   switch (udxSap->state)
+   {
+      case RLC_SAP_BINDING:
+      {
+         if(TRUE == rlcChkTmr(tRlcCb,(PTR)udxSap,EVENT_RLC_WAIT_BNDCFM))
+         {
+             rlcStopTmr (tRlcCb,(PTR)udxSap, EVENT_RLC_WAIT_BNDCFM);
+         }
+         udxSap->retryCnt = 0;
+          
+         if (status == CM_BND_OK)
+         {
+            udxSap->state = RLC_SAP_BND;
+            event = LCM_EVENT_BND_OK;
+            cause = LKW_CAUSE_SAP_BNDENB;
+         }
+         else
+         {
+            udxSap->state = RLC_SAP_CFG;
+            event = LCM_EVENT_BND_FAIL;
+            cause = LKW_CAUSE_UNKNOWN;
+         }
+      }
+      break;
+
+     default:
+        event = LKW_EVENT_RGU_BND_CFM;
+        cause = LCM_CAUSE_INV_STATE;
+        break;
+   }
+   /* Send an alarm with proper event and cause */
+   RLC_SEND_SAPID_ALARM(tRlcCb,suId, event, cause);
+
+   return ROK;
+} 
+
+/**
+ * @brief 
+ *    Handles UDX Configuration Confirm 
+ *
+ * @details
+ *    This function handles the UDX configuration Confirm from DL Instance
+ *
+ * @param[in] pst     Post structure  
+ * @param[in] suId    Service User ID
+ * @param[in] cfmInfo Confirm Information 
+ * 
+ * @return  S16
+ *    -# ROK 
+ *    -# RFAILED
+ */
+S16 rlcUlUdxCfgCfm
+(
+Pst             *pst,  
+SuId            suId, 
+RlcCfgCfmInfo   *cfmInfo  
+)
+{
+   RlcCb             *tRlcCb;
+   RlcCfgCfmInfo    *cfgCfm;
+   RlcUlCfgTmpData   *cfgTmpData;
+   Pst rspPst;
+   memset(&rspPst, 0, sizeof(Pst));
+
+#if (ERRCLASS & ERRCLS_INT_PAR)
+   if (pst->dstInst >= MAX_RLC_INSTANCES)
+   {
+      RLC_FREE_SHRABL_BUF(pst->region,
+                         pst->pool,
+	                 cfmInfo,
+                         sizeof(RlcCfgCfmInfo));
+      return  (RFAILED);
+   }
+#endif
+   tRlcCb = RLC_GET_RLCCB(pst->dstInst);
+   RLCDBGP_BRIEF(tRlcCb, " suId(%d)\n", suId);
+
+#if (ERRCLASS & ERRCLS_INT_PAR)
+   if (suId < 0)
+   {
+      DU_LOG("\nERROR  -->  RLC_UL : Invalid suId");
+      RLC_SEND_SAPID_ALARM(tRlcCb,suId, 
+                           LKW_EVENT_LI_BND_CFM, LCM_CAUSE_INV_SUID);
+      RLC_FREE_SHRABL_BUF(pst->region,
+                         pst->pool,
+	                 cfmInfo,
+                         sizeof(RlcCfgCfmInfo));
+      return RFAILED;
+   }
+#endif /* ERRCLASS & ERRCLS_INT_PAR */
+
+   if(ROK != rlcDbmFindUlTransaction(tRlcCb,cfmInfo->transId, &cfgTmpData))
+   {
+      DU_LOG("\nERROR  -->  RLC_UL : Invalid transId");
+      RLC_FREE_SHRABL_BUF(pst->region,
+                         pst->pool,
+	                 cfmInfo,
+                         sizeof(RlcCfgCfmInfo));
+      return  (RFAILED);
+   }
+
+   if(ROK != rlcDbmDelUlTransaction(tRlcCb, cfgTmpData))
+   {
+      RLC_FREE_SHRABL_BUF(pst->region,
+                         pst->pool,
+	                 cfmInfo,
+                         sizeof(RlcCfgCfmInfo));
+       return RFAILED;
+   }
+      /* Allocate memory and memset to 0 for cfmInfo */
+   RLC_ALLOC(tRlcCb,cfgCfm, sizeof(RlcCfgCfmInfo));
+#if (ERRCLASS & ERRCLS_ADD_RES)
+   if (cfgCfm == NULLP)
+   {
+      DU_LOG("\nERROR  -->  RLC_UL : Memory Allocation failed.");
+      RLC_FREE_SHRABL_BUF(pst->region,
+                         pst->pool,
+	                 cfmInfo,
+                         sizeof(RlcCfgCfmInfo));
+       return RFAILED;
+   }
+#endif /* ERRCLASS & ERRCLS_ADD_RES */
+   rlcHdlCrlcUlCfgReq(tRlcCb,cfgTmpData, cfmInfo, cfgCfm);
+   if(tRlcCb->u.ulCb->rlcUlUdxEventType == EVENT_RLC_UE_CREATE_REQ)
+   {
+      FILL_PST_RLC_TO_DUAPP(rspPst, RLC_UL_INST, EVENT_RLC_UE_CREATE_RSP);
+   }
+   else if(tRlcCb->u.ulCb->rlcUlUdxEventType == EVENT_RLC_UE_RECONFIG_REQ)
+   {
+      FILL_PST_RLC_TO_DUAPP(rspPst, RLC_UL_INST, EVENT_RLC_UE_RECONFIG_RSP);
+   }
+   SendRlcUeRspToDu(&rspPst, cfgCfm);
+
+   /* free the memory from DL */
+   RLC_FREE_SHRABL_BUF(pst->region,
+                      pst->pool,
+		      cfmInfo,
+		      sizeof(RlcCfgCfmInfo));
+
+   /* free the cfgInfo that came from LM */
+   RLC_PST_FREE(pst->region, pst->pool, cfgTmpData->cfgInfo, sizeof(RlcCfgInfo));
+   RLC_FREE(tRlcCb,cfgTmpData,sizeof(RlcUlCfgTmpData));
+   
+   return ROK;
+} 
+
+/**
+ * @brief 
+ *    Handler for UeId change confirm 
+ *
+ * @details
+ *    This function handles the UeId Change Confirm from DL Instance 
+ *
+ *  @param[in] pst     Post structure  
+ *  @param[in] suId    Service User ID
+ *  @param[in] transId Transaction Id 
+ *  @param[in] status  Status of confirmation 
+ *  @return  S16
+ *      -# ROK 
+ *      -# RFAILED
+ */
+
+S16 rlcUlUdxUeIdChgCfm
+(
+Pst        *pst,          
+SuId       suId,           
+uint32_t        transId,
+CmStatus   status
+)
+{
+   RlcCb             *tRlcCb;
+   RlcUlCfgTmpData   *cfgTmpData;
+
+#if (ERRCLASS & ERRCLS_INT_PAR)
+   if (pst->dstInst >= MAX_RLC_INSTANCES)
+   {
+      return  (RFAILED);
+   }
+#endif
+   tRlcCb = RLC_GET_RLCCB(pst->dstInst);
+
+   RLCDBGP_BRIEF(tRlcCb, " suId(%d) \n", suId);
+
+#if (ERRCLASS & ERRCLS_INT_PAR)
+   if (suId < 0)
+   {
+      DU_LOG("\nERROR  -->  RLC_UL : Invalid suId");
+      return RFAILED;
+   }
+#endif /* ERRCLASS & ERRCLS_INT_PAR */
+
+   if(ROK != rlcDbmFindUlTransaction(tRlcCb, transId, &cfgTmpData))
+   {
+      DU_LOG("\nERROR  -->  RLC_UL : Invalid transId");
+      return  (RFAILED);
+   }
+
+   if(ROK != rlcDbmDelUlTransaction(tRlcCb, cfgTmpData))
+   {
+      return RFAILED;
+   }
+
+   if(status.status == CKW_CFG_CFM_OK)
+   {
+       if(cfgTmpData->ueCb != NULLP) 
+       {
+      rlcCfgApplyUlUeIdChng(tRlcCb, cfgTmpData->ueInfo, cfgTmpData->newUeInfo, cfgTmpData);
+   }
+   }
+   RlcUiCkwUeIdChgCfm(&(tRlcCb->u.ulCb->ckwSap.pst),
+                     tRlcCb->u.ulCb->ckwSap.suId,
+                     transId,cfgTmpData->ueInfo,status);
+   /* only newUeInfo needs to be freed here, ueInfo would be freed at the 
+      interface or by he receipient in case of tight coupling */
+   RLC_PST_FREE(pst->region, pst->pool, cfgTmpData->newUeInfo, sizeof(CkwUeInfo));
+   RLC_FREE(tRlcCb, cfgTmpData, sizeof (RlcUlCfgTmpData));
+   return ROK;
+} 
+
+/**
+ * @brief 
+ *    Udx Status Prohibit Timer Start
+ *
+ *  @param[in] pst     Post structure  
+ *  @param[in] suId    Service User ID
+ *  @param[in] rlcId   rlc Id 
+ *  @return  S16
+ *      -# ROK 
+ *      -# RFAILED
+ */
+S16  rlcUlUdxStaProhTmrStart
+(
+Pst*         pst,
+SuId         suId,
+CmLteRlcId   *rlcId
+)
+{
+   RlcCb       *tRlcCb= NULLP;
+   RlcUlRbCb   *rbCb;        
+
+#if (ERRCLASS & ERRCLS_INT_PAR)
+   if (pst->dstInst >= MAX_RLC_INSTANCES)
+   {
+      return  (RFAILED);
+   }
+#endif
+   tRlcCb = RLC_GET_RLCCB(pst->dstInst);
+
+   rlcDbmFetchUlRbCbByRbId(tRlcCb, rlcId, &rbCb);
+   if (rbCb == NULLP)
+   {    
+      DU_LOG("\nERROR  -->  RLC_UL : CellId [%u]:RbId[%d] not found",
+            rlcId->cellId,rlcId->rbId);
+      return RFAILED;
+   }
+
+   /* Start staProhTmr */
+   rlcStartTmr(tRlcCb,(PTR)rbCb, EVENT_RLC_AMUL_STA_PROH_TMR);
+
+   return  (ROK);
+} 
+
+/**
+ * @brief 
+ *    Handler for configuration confirm from DL.
+ *
+ * @param[in] gCb        - RLC Instance Control Block
+ * @param[in] cfgTmpData - Configuration Temporary Data 
+ * @param[in] cfmInfo    - DL Configuration Confirm Info
+ * @param[out] cfgCfm    - Configuration Confirm to be sent to RRC
+ *
+ * @return  S16
+ *    -# ROK 
+ *    -# RFAILED
+ */
+S16 rlcHdlCrlcUlCfgReq
+(
+RlcCb             *gCb,
+RlcUlCfgTmpData   *cfgTmpData,
+RlcCfgCfmInfo    *cfmInfo,
+RlcCfgCfmInfo    *cfgCfm
+)
+{
+   RlcCfgInfo   *cfg;
+   uint32_t     idx;
+   uint32_t     maxEnt;
+   
+   cfg = cfgTmpData->cfgInfo;
+   maxEnt = (cfg->numEnt < CKW_MAX_ENT_CFG)? cfg->numEnt:CKW_MAX_ENT_CFG;
+
+   for (idx = 0; idx < maxEnt; idx++)
+   {
+      RlcEntCfgCfmInfo   *entCfgCfm;
+      RlcEntCfgCfmInfo   *entDlCfgCfm;
+      RlcEntCfgInfo      *entCfg;
+
+      entCfg  = &(cfg->entCfg[idx]);
+      entCfgCfm   = &(cfgCfm->entCfgCfm[idx]);
+      entDlCfgCfm = &(cfmInfo->entCfgCfm[idx]);
+      switch (entCfg->cfgType)
+      {
+         case CKW_CFG_ADD:
+         case CKW_CFG_MODIFY:
+         case CKW_CFG_DELETE:
+            {
+               if (entCfg->dir == RLC_DIR_UL)
+               {
+                  RLC_MEM_CPY(entCfgCfm, 
+                             &cfgTmpData->cfgEntData[idx].entUlCfgCfm, 
+                             sizeof(RlcEntCfgCfmInfo)); 
+               }
+               else if (entCfg->dir == RLC_DIR_DL)
+               {
+                  RLC_MEM_CPY(entCfgCfm, entDlCfgCfm, sizeof(RlcEntCfgCfmInfo)); 
+               }
+               else if(entCfg->dir == RLC_DIR_BOTH)
+               {
+                  if (entDlCfgCfm->status.status != CKW_CFG_CFM_OK)
+                  {
+                     rlcCfgRollBackUlRb(gCb,
+                                       cfg->ueId,
+                                       &cfg->entCfg[idx], 
+                                       &cfgTmpData->cfgEntData[idx]);
+                  }
+                  else
+                  {
+                     rlcCfgApplyUlRb(gCb,  
+                                    &cfg->entCfg[idx], 
+                                    &cfgTmpData->cfgEntData[idx],
+                                    cfgTmpData);
+                  }
+                  RLC_MEM_CPY(entCfgCfm, entDlCfgCfm, sizeof(RlcEntCfgCfmInfo)); 
+               }
+               else
+               {
+		            RLC_CFG_FILL_CFG_CFM(entCfgCfm, entCfg->rbId, entCfg->rbType,
+				           CKW_CFG_CFM_NOK, CKW_CFG_REAS_INVALID_DIR);
+                  DU_LOG("\nDEBUG  -->  RLC_UL : RBID[%d] direction[%d] is invalid",
+                         entCfg->rbId,entCfg->dir);
+               }
+               break;
+            }
+
+         case CKW_CFG_REESTABLISH:
+            {
+               if (entCfg->dir == RLC_DIR_UL)
+               {
+                  RLC_MEM_CPY(entCfgCfm, 
+                             &cfgTmpData->cfgEntData[idx].entUlCfgCfm, 
+                             sizeof(RlcEntCfgCfmInfo)); 
+               }
+               else if (entCfg->dir == RLC_DIR_DL)
+               {
+                  RLC_MEM_CPY(entCfgCfm, entDlCfgCfm, sizeof(RlcEntCfgCfmInfo)); 
+               }
+               else
+               {
+                  if (entCfg->dir & RLC_DIR_UL)
+                  {
+                     /* Reestablish indication is sent from UL Instance only*/
+                     if (entDlCfgCfm->status.status == CKW_CFG_CFM_OK)
+                     {
+                        rlcCfgApplyReEstUlRb (gCb, cfg->ueId, 
+                                             cfg->cellId, TRUE, 
+                                             &cfgTmpData->cfgEntData[idx]);
+                     }
+                     RLC_MEM_CPY(entCfgCfm, entDlCfgCfm, sizeof(RlcEntCfgCfmInfo)); 
+                  }
+               }
+               break;
+            }
+         case CKW_CFG_DELETE_UE:
+            {
+               if (cfgTmpData->cfgEntData[idx].entUlCfgCfm.status.status == CKW_CFG_CFM_OK)
+               {
+                  rlcCfgApplyDelUlUe(gCb, cfgTmpData);
+                  RLC_MEM_CPY(entCfgCfm, 
+                             &cfgTmpData->cfgEntData[idx].entUlCfgCfm, 
+                             sizeof(RlcEntCfgCfmInfo)); 
+               }
+               else
+               {
+                   RLC_MEM_CPY(entCfgCfm, entDlCfgCfm,
+                              sizeof(RlcEntCfgCfmInfo)); 
+               }
+               break;
+            }
+         case CKW_CFG_DELETE_CELL:
+            {
+               if (cfgTmpData->cfgEntData[idx].entUlCfgCfm.status.status == CKW_CFG_CFM_OK)
+               {
+                  rlcCfgApplyDelUlCell(gCb, cfgTmpData);
+                  RLC_MEM_CPY(entCfgCfm, 
+                             &cfgTmpData->cfgEntData[idx].entUlCfgCfm, 
+                             sizeof(RlcEntCfgCfmInfo)); 
+               }
+               else
+               {
+                   RLC_MEM_CPY(entCfgCfm, entDlCfgCfm,
+                              sizeof(RlcEntCfgCfmInfo)); 
+               }
+               break;
+            }
+         default:
+            {
+               RLC_CFG_FILL_CFG_CFM(entCfgCfm, entCfg->rbId, entCfg->rbType,
+                                   CKW_CFG_CFM_NOK, CKW_CFG_REAS_INVALID_CFG);
+
+               DU_LOG("\nERROR  -->  RLC_UL : Invalid configuration type");
+            }
+      }
+   }
+
+   /* Assign number of entity configurations and suId */
+   cfgCfm->transId = cfgTmpData->uprLyrTransId;
+   cfgCfm->ueId = cfg->ueId;
+   cfgCfm->cellId = cfg->cellId;
+   cfgCfm->numEnt = cfg->numEnt;
+
+   return ROK;
+}
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+
+/**********************************************************************
+         End of file
+**********************************************************************/
---
+++ src/5gnrrlc/rlc_tmm_ul.c
@@ -0,0 +1,308 @@
+/*******************************************************************************
+################################################################################
+#   Copyright (c) [2017-2019] [Radisys]                                        #
+#                                                                              #
+#   Licensed under the Apache License, Version 2.0 (the "License");            #
+#   you may not use this file except in compliance with the License.           #
+#   You may obtain a copy of the License at                                    #
+#                                                                              #
+#       http://www.apache.org/licenses/LICENSE-2.0                             #
+#                                                                              #
+#   Unless required by applicable law or agreed to in writing, software        #
+#   distributed under the License is distributed on an "AS IS" BASIS,          #
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+#   See the License for the specific language governing permissions and        #
+#   limitations under the License.                                             #
+################################################################################
+*******************************************************************************/
+
+/**********************************************************************
+
+     Name:     NR RLC Layer 
+  
+     Type:     C file
+  
+     Desc:     Source code for RLC Transparent mode assembly and
+               reassembly.This file contains following functions
+                
+                  --rlcTmmQSdu
+                  --rlcTmmSendToMac
+                  --rlcTmmRcvFrmMac
+                  --kwTmmReEstablish 
+
+     File:     rlc_tmm_ul.c
+
+**********************************************************************/
+
+/** @file rlc_tmm_ul.c
+@brief RLC Transparent Mode module
+*/
+
+/* header (.h) include files */
+#include "common_def.h"
+#include "lkw.h"           /* LKW defines */
+#include "ckw.h"           /* CKW defines */
+#include "kwu.h"           /* KWU defines */
+#include "rgu.h"           /* RGU defines */
+#include "rlc_env.h"        /* RLC environment options */
+
+#include "rlc_utils.h"            /* RLC defines */
+#include "rlc_err.h"            /* RLC defines */
+#include "rlc_ul.h"
+
+/* extern (.x) include files */
+#include "lkw.x"           /* LKW */
+#include "ckw.x"           /* CKW */
+#include "kwu.x"           /* KWU */
+#include "rgu.x"           /* RGU */
+
+#include "rlc_utils.x"
+#include "rlc_ul.x"
+#include "du_app_rlc_inf.h"
+#include "rlc_upr_inf_api.h"
+
+#if defined(PRE_DEF_UE_CTX) || defined(PRE_DEF_UE_CTX_HO)
+#ifdef EG_GEN_LOAD_5GTF
+uint32_t loadStart;
+#endif
+
+S16 PjUiPjuDatInd(Pst* pst,SuId suId,CmLtePdcpId * pdcpId,Buffer *mBuf);
+Void AddUeContext
+(
+CmLteRnti   crnti,
+uint8_t rrcMsgType
+)
+{
+   KwuDatIndInfo   *datIndInfo;   /* Data Indication Information */
+   uint8_t rrcConReq[6] ={ 0x50, 0x30, 0x30, 0x30, 0x30, 0x34 };
+   uint8_t rrcConSetupComplete[34] ={ 0x20, 0x00, 0x3e, 0x0e, 0x82, 0x02, 0x10, 0x12, 0x20, 0x02, 0x20, 0x64, 0xa8, 0x2c, 0x48, 0x05, 0x00, 0x80, 0x00, 0x08, 0x04, 0x03, 0xa0, 0x02, 0xa0, 0x10, 0x12, 0x20, 0x02, 0x20, 0x64, 0xa8, 0x2c, 0x48};
+#ifndef CA_PAL_5GTF 
+   uint8_t rrcUeCapabilityInfo[12] ={0x38, 0x01, 0x00, 0x80, 0x1b, 0xff, 0x0c, 0x00, 0x20, 0x00, 0x80, 0x00};
+#else
+  /* uint8_t rrcUeCapabilityInfo[44] ={0x38,0x01,0x02,0x84,0x9b,0xff,0x0c,0x00,0x20,0x00,0x80,0x1f,0xfe,0xf4,0x4f,0xe0,0x40,0x03,0x80,0x11,0x04,0x0c,0x20,0x88,0x20,0x7f,0xff,0xff,0xff,0xf3,0xff,0x81,0xff,0xff,0xff,0xff,0x7f,0xf0,0x3f,0xff,0xff,0xff,0xe0,0x00};
+*/
+uint8_t rrcUeCapabilityInfo[] = 
+{
+ 0x38,0x01,0x03,0x34,0x9b,0xff,0x0c,0x00,0x20,0x00,0x80,0x1f,0xfe,0xf4,0x4f,0xe0
+ ,0x40,0x09,0x80,0x11,0x04,0x0c,0x20,0x88,0x20,0x63,0x04,0x41,0x03,0x20,0x22,0x08
+ ,0x19,0x61,0x10,0x40,0xff,0xff,0xff,0xff,0xe7,0xff,0x03,0xff,0xff,0xff,0xfe,0xff
+ ,0xe0,0x7f,0xff,0xff,0xff,0xc0,0x00
+};
+
+#endif
+   uint8_t rrcSecurityModeComplete[2] ={0x28, 0x80};
+   uint8_t rrcReconfigComplete[2] ={0x10, 0x80};
+
+   Buffer *pdu;
+
+   Pst ulPst1 ={100,100,217,0,215,0,PRIOR0,0,81,1,1,0,0};
+#ifdef PJ
+   Pst ulPst2 ={100,100,217,0,216,0,PRIOR0,0,68,0,1,0,0};
+#endif
+
+  if(1 == rrcMsgType)
+  {
+
+   KwuDatIndInfo datIndInfoTmp;
+   datIndInfo = &datIndInfoTmp;
+   datIndInfo->rlcId.rbId = 5;
+   datIndInfo->rlcId.rbType = 0;
+   datIndInfo->rlcId.ueId = 0;
+   datIndInfo->rlcId.cellId = 1;
+   datIndInfo->tCrnti = crnti;
+   datIndInfo->isOutOfSeq = 16;
+
+   SGetMsg(DFLT_REGION, DFLT_POOL, (Buffer **) &pdu);
+  
+   SAddPstMsgMult(rrcConReq,6,pdu);
+
+   DU_LOG("\nINFO  -->  RLC_UL : Profiling Framework Sending RRC Connection Req to RRC for UE :%d\n",crnti);
+   rlcSendUlDataToDu(&ulPst1, datIndInfo, pdu);
+ }
+ else if(2 == rrcMsgType)
+ {
+#ifdef PJ
+    CmLtePdcpId pdcpId;
+    pdcpId.cellId = 1;
+    pdcpId.ueId = crnti;
+    pdcpId.rbId = 1;
+    pdcpId.rbType = 0;
+#endif   
+    SGetMsg(DFLT_REGION, DFLT_POOL, (Buffer **) &pdu);
+  
+    SAddPstMsgMult(rrcConSetupComplete,34,pdu);
+
+    DU_LOG("\nINFO  -->  RLC_UL : Profiling Framework: Sending RRC Connection Setup Complete to RRC for UE :%d\n",crnti);
+#ifdef PJ
+    PjUiPjuDatInd(&ulPst2, 1, &pdcpId, pdu);
+#endif
+ }
+ 
+ else if(3 == rrcMsgType)
+ {
+#ifdef PJ
+    CmLtePdcpId pdcpId;
+    pdcpId.cellId = 1;
+    pdcpId.ueId = crnti;
+    pdcpId.rbId = 1;
+    pdcpId.rbType = 0;
+#endif   
+    SGetMsg(DFLT_REGION, DFLT_POOL, (Buffer **) &pdu);
+  
+#ifndef CA_PAL_5GTF 
+    SAddPstMsgMult(rrcUeCapabilityInfo,12,pdu);
+#else    
+    SAddPstMsgMult(rrcUeCapabilityInfo,sizeof(rrcUeCapabilityInfo),pdu);
+#endif
+    DU_LOG("\nINFO  -->  RLC_UL : Profiling Framework: Sending RRC UE Capability Info to RRC for UE :%d\n",crnti);
+#ifdef PJ
+    PjUiPjuDatInd(&ulPst2, 1, &pdcpId, pdu);
+#endif
+
+ }
+ else if(4 == rrcMsgType)
+ {
+#ifdef PJ
+    CmLtePdcpId pdcpId;
+    pdcpId.cellId = 1;
+    pdcpId.ueId = crnti;
+    pdcpId.rbId = 1;
+    pdcpId.rbType = 0;
+#endif
+   
+    SGetMsg(DFLT_REGION, DFLT_POOL, (Buffer **) &pdu);
+  
+    SAddPstMsgMult(rrcSecurityModeComplete,2,pdu);
+
+    DU_LOG("\nINFO  -->  RLC_UL : Profiling Framework: Sending RRC Security Mode Complete to RRC for UE :%d\n",crnti);
+#ifdef PJ
+    PjUiPjuDatInd(&ulPst2, 1, &pdcpId, pdu);
+#endif
+ }
+ else if(5 == rrcMsgType)
+ {
+#ifdef PJ
+    CmLtePdcpId pdcpId;
+    pdcpId.cellId = 1;
+    pdcpId.ueId = crnti;
+    pdcpId.rbId = 1;
+    pdcpId.rbType = 0;
+#endif
+   
+    SGetMsg(DFLT_REGION, DFLT_POOL, (Buffer **) &pdu);
+  
+    SAddPstMsgMult(rrcReconfigComplete,2,pdu);
+
+    DU_LOG("\nINFO  -->  RLC_UL : Profiling Framework: Sending RRC Reconfig Complete to RRC for UE :%d\n",crnti);
+#ifdef PJ
+    PjUiPjuDatInd(&ulPst2, 1, &pdcpId, pdu);
+#ifdef EG_GEN_LOAD_5GTF
+    loadStart=1;
+#endif
+#endif
+ }
+ return;
+}
+#endif 
+/** @addtogroup tmmode */
+/*@{*/
+
+#define RLC_MODULE (RLC_DBGMASK_TM | RLC_DBGMASK_UL)
+
+/**
+ * @brief
+ *    Handler to process the Data Indication from the lower layer and send the 
+ *    SDU to upper layer.
+ *
+ * @details
+ *    This function processes the PDU receivied from the lower layer and sends 
+ *    the same pdu as SDU to the upper layer.It sends CRNTI along with the SDU 
+ *    for CCCH.
+ *            
+ * @param[in] gCb     RLC Instance Control Block
+ * @param[in] rbCb    RB control block. 
+ * @param[in] tCrnti  TCRNTI  
+ * @param[in] pdu     PDU
+ * 
+ * @return  S16
+ *    -# ROK 
+ *    -# RFAILED 
+ */
+#ifdef CCPU_OPT
+void rlcTmmRcvFrmMac(RlcCb *gCb, RlcUlRbCb *rbCb, CmLteRnti tCrnti, Buffer *pdu)
+#else
+void rlcTmmRcvFrmMac(RlcCb *gCb, RlcUlRbCb *rbCb, Buffer *pdu)
+#endif
+{
+   RlcUlRrcMsgInfo  *ulRrcMsgInfo;
+   uint16_t         msgLen;
+   uint16_t         copyLen;    /* Number of bytes copied */
+   Pst              pst;
+ 
+   gCb->genSts.pdusRecv++;
+   ODU_GET_MSG_LEN(pdu, (MsgLen *)&msgLen);
+   gCb->genSts.bytesRecv += msgLen;
+  
+   /* Filling UL RRC Message Info */
+   RLC_ALLOC_SHRABL_BUF(RLC_MEM_REGION_UL, RLC_POOL,
+      ulRrcMsgInfo, sizeof(RlcUlRrcMsgInfo));
+   if (ulRrcMsgInfo)
+   {
+      ulRrcMsgInfo->cellId = rbCb->rlcId.cellId;
+      ulRrcMsgInfo->ueIdx = rbCb->rlcId.ueId;
+      ulRrcMsgInfo->lcId = rbCb->lch.lChId;
+      RLC_ALLOC_SHRABL_BUF(RLC_MEM_REGION_UL, RLC_POOL,
+         ulRrcMsgInfo->rrcMsg, msgLen);
+      if (ulRrcMsgInfo->rrcMsg)
+      {
+         ODU_COPY_MSG_TO_FIX_BUF(pdu, 0, msgLen, ulRrcMsgInfo->rrcMsg, (MsgLen *)&copyLen);
+         ulRrcMsgInfo->msgLen = msgLen;
+
+         /* Sending UL RRC Message transfeer to DU APP */
+         memset(&pst, 0, sizeof(Pst));
+         FILL_PST_RLC_TO_DUAPP(pst, RLC_UL_INST, EVENT_UL_RRC_MSG_TRANS_TO_DU);
+         rlcSendUlRrcMsgToDu(&pst, ulRrcMsgInfo);
+      }
+      else
+      {
+         DU_LOG("\nERROR  -->  RLC_UL : rlcTmmRcvFrmMac: Memory allocation failed for UL RRC Msg");
+      }
+   }
+   else
+   {
+      DU_LOG("\nERROR  -->  RLC_UL : rlcTmmRcvFrmMac: Memory allocation failed for ulRrcMsgInfo");
+   }
+ 
+   return;
+}
+
+/**
+ * @brief 
+ *    Handler to process the re-establishment request received from the upper 
+ *    layer. It is just a hook for future enhancement. 
+ *
+ *            
+ * @param[in] gCb   RLC Instance Control Block
+ * @param[in] rbCb  RB control block. 
+ *
+ * @return  S16
+ *    -# ROK 
+ *       
+ **/
+Void rlcTmmUlReEstablish
+(
+RlcCb        *gCb,
+RlcUlRbCb    *rbCb        
+)
+{
+   DU_LOG("\nINFO  -->  RLC_UL : do nothing for TMM for ReEstablish");
+   return;
+}
+
+#ifdef _cplusplus
+}
+#endif
+/*@}*/
+/********************************************************************30**
+         End of file
+**********************************************************************/
