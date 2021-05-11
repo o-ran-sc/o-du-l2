b4e4de86f23f38ac4b4eac171e4fbafcd426110a
sphoorthi <sphoorthi.dayanand@radisys.com>
2021-05-11 14:43:00 +0530

    <JIRA ID: ODUHIGH- : Implementation of resource bitmap in schDlSlotInfo & schUlSlotInfo>
    Change-Id: Id42e790071d90dd93b1fc4038f939878e5587ffd
    Signed-off-by: sphoorthi <sphoorthi.dayanand@radisys.com>

--- src/5gnrsch/sch_common.c
+++ src/5gnrsch/sch_common.c
@@ -197,35 +197,41 @@ void schPrachResAlloc(SchCellCb *cell, UlSchedInfo *ulSchedInfo, SlotIndInfo pra
       /* check for subFrame number */
       if ((1 << prachOccasionTimingInfo.slot) & prachSubframe)
       {
-	 /* prach ocassion present in this subframe */
+         /* prach ocassion present in this subframe */
 
-	 prachFormat      = prachCfgIdxTable[prachCfgIdx][0];
-	 prachStartSymbol = prachCfgIdxTable[prachCfgIdx][4];
-	 prachOcas        = prachCfgIdxTable[prachCfgIdx][6];
+         prachFormat      = prachCfgIdxTable[prachCfgIdx][0];
+         prachStartSymbol = prachCfgIdxTable[prachCfgIdx][4];
+         prachOcas        = prachCfgIdxTable[prachCfgIdx][6];
 
-	 /* freq domain resource determination for RACH*/
-	 freqStart = cell->cellCfg.schRachCfg.msg1FreqStart;
-	 /* numRa determined as 𝑛 belonging {0,1,.., M − 1}, 
-	  * where M is given by msg1Fdm */
-	 numRa = (cell->cellCfg.schRachCfg.msg1Fdm - 1);
-	 for(idx=0; idx<MAX_RACH_NUM_RB_IDX; idx++)
-	 {
-	    if(numRbForPrachTable[idx][0] == cell->cellCfg.schRachCfg.rootSeqLen)
-	    {
-	       if(numRbForPrachTable[idx][1] == cell->cellCfg.schRachCfg.prachSubcSpacing)
-	       {
-		  if(numRbForPrachTable[idx][2] == puschScs)
-		  {
-		     break;
-		  }
-	       }
-	    }
-	 }
-	 numPrachRb = numRbForPrachTable[idx][3];
-	 dataType |= SCH_DATATYPE_PRACH;
-	 /* Considering first slot in the frame for PRACH */
-	 idx = 0;
-	 schUlSlotInfo->assignedPrb[idx] = freqStart+numPrachRb;
+            /* freq domain resource determination for RACH*/
+         freqStart = cell->cellCfg.schRachCfg.msg1FreqStart;
+         /* numRa determined as 𝑛 belonging {0,1,.., M − 1}, 
+          * where M is given by msg1Fdm */
+         numRa = (cell->cellCfg.schRachCfg.msg1Fdm - 1);
+         for(idx=0; idx<MAX_RACH_NUM_RB_IDX; idx++)
+         {
+            if(numRbForPrachTable[idx][0] == cell->cellCfg.schRachCfg.rootSeqLen)
+            {
+               if(numRbForPrachTable[idx][1] == cell->cellCfg.schRachCfg.prachSubcSpacing)
+               {
+                  if(numRbForPrachTable[idx][2] == puschScs)
+                  {
+                     break;
+                  }
+               }
+            }
+         }
+         numPrachRb = numRbForPrachTable[idx][3];
+         dataType |= SCH_DATATYPE_PRACH;
+         /* Considering first slot in the frame for PRACH */
+         idx = 0;
+         schUlSlotInfo->assignedPrb[idx] = freqStart+numPrachRb;
+
+         if(fillResourceBitmap(schUlSlotInfo->resAllocBitMap[idx], freqStart, numPrachRb))
+         {
+            /*ResourceAllocation failure
+            return RFAILED;*/
+         }
       }
       ulSchedInfo->dataType = dataType;
       /* prach info */
