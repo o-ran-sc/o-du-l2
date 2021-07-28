e3799f02b83bf3e8ab6818c14cdd5dd870e8d552
lal.harshita <Harshita.Lal@radisys.com>
2021-07-28 11:42:28 +0530

    adding sctp lib dependency.
    Signed-off-by: lal.harshita <Harshita.Lal@radisys.com>
    Change-Id: I3238c95d0b97f920c1379444916a475dcd4a61e3

--- test/unit_test.sh
+++ test/unit_test.sh
@@ -1,1 +1,1 @@
-cd odu;make clean_odu MACHINE=BIT64 MODE=TDD;make odu MACHINE=BIT64 MODE=TDD COVERAGE=1
+sudo apt install -y libsctp-dev;cd odu;make clean_odu MACHINE=BIT64 MODE=TDD;make odu MACHINE=BIT64 MODE=TDD COVERAGE=1
