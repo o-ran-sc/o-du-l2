6870f21924ad1a4cdcd181d0b5b51cc850bd372d
barveankit <anbarve@radisys.com>
2022-01-13 23:27:38 +0530

    Replacing E2AP v1.0 asn files with E2AP v2.0 files [Jira Id - ODUHIGH-409]
    Signed-off-by: barveankit <anbarve@radisys.com>
    Change-Id: I6c3f6f95ff2236d66325e741dad87dba93e3fc7a
    Signed-off-by: barveankit <anbarve@radisys.com>

--- src/codec_utils/common/asn_constant.h
+++ src/codec_utils/common/asn_constant.h
@@ -9,6 +9,13 @@
 extern "C" {
 #endif
 
+#define maxProtocolIEs (65535)
+#define maxnoofErrors (256)
+#define maxofE2nodeComponents (1024)
+#define maxofRANfunctionID (256)
+#define maxofRICactionID (16)
+#define maxofTNLA (32)
+#define maxofRICrequestID (4294967295)
 #define maxSIB (32)
 #define maxNrofPhysicalResourceBlocks (275)
 #define maxNrofPhysicalResourceBlocks_1 (274)
