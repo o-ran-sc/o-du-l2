2a176c02d6347bd57ab1a05f336d176eec6cf196
lal.harshita <Harshita.Lal@radisys.com>
2022-12-09 11:08:24 +0530

    Fix: Sonarcube sctp missing dependency
    Change-Id: I4be06f514640e10603bc7e35c0a35062b5aa441c
    Signed-off-by: lal.harshita <Harshita.Lal@radisys.com>
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- test/unit_test.sh
+++ test/unit_test.sh
@@ -1,1 +1,12 @@
-sudo apt install -y libsctp-dev;cd odu;make clean_odu MACHINE=BIT64 MODE=TDD;make odu MACHINE=BIT64 MODE=TDD COVERAGE=1
+#!/bin/bash
+
+set -euxo pipefail
+
+echo "---> unit_test.sh"
+
+echo "Present working directory: "; pwd
+echo "Installing libsctp-dev package dependency..."
+sudo apt-get update
+sudo apt-get install -y libsctp-dev
+echo "Running job"
+cd odu;make clean_odu MACHINE=BIT64 MODE=TDD;make odu MACHINE=BIT64 MODE=TDD COVERAGE=1
