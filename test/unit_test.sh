7e69f464b70b4c75a5147e2a3ff157b6645ab0d7
lal.harshita <Harshita.Lal@radisys.com>
2022-12-09 11:08:24 +0530

    sonarcube sctp fix
    Signed-off-by: lal.harshita <Harshita.Lal@radisys.com>
    Change-Id: I4be06f514640e10603bc7e35c0a35062b5aa441c
    Signed-off-by: lal.harshita <Harshita.Lal@radisys.com>

--- test/unit_test.sh
+++ test/unit_test.sh
@@ -1,1 +1,11 @@
-sudo apt install -y libsctp-dev;cd odu;make clean_odu MACHINE=BIT64 MODE=TDD;make odu MACHINE=BIT64 MODE=TDD COVERAGE=1
+#!/bin/bash
+
+
+set -euxo pipefail
+echo "Hello world!"
+sudo apt-get update
+sudo apt-cache search libsctp-dev || :
+sudo apt-cache showpkg libsctp-dev || :
+sudo apt-get install -y libsctp-dev
+sudo dpkg-query -l
+cd odu;make clean_odu MACHINE=BIT64 MODE=TDD;make odu MACHINE=BIT64 MODE=TDD COVERAGE=1
