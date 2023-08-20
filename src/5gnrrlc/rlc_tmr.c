23c1372f6a4876e4221d08a7a5eaf0f32a94d33f
frbimo2 <d11002806@gapps.ntust.edu.tw>
2023-08-20 17:15:33 +0800

    Support the advanced feature of slice-based scheduling
    Change-Id: Ied123dee03c2ed9cedb1174b087e1feee9e90047
    Signed-off-by: frbimo2 <d11002806@gapps.ntust.edu.tw>

--- src/5gnrrlc/rlc_tmr.c
+++ src/5gnrrlc/rlc_tmr.c
@@ -202,10 +202,19 @@ void rlcStartTmr(RlcCb *gCb, PTR cb, int16_t tmrEvnt)
          arg.max = RLC_MAX_THPT_TMR; 
          break;
       }
+      case EVENT_RLC_DRB_THROUGHPUT_TMR:
+      {
+         RlcThpt *thptCb = (RlcThpt *)cb;
+         RLC_TMR_CALCUATE_WAIT(arg.wait, ODU_DRB_THROUGHPUT_PRINT_TIME_INTERVAL, gCb->genCfg.timeRes);
+         arg.timers = &thptCb->drbTputInfo.drbThptTmr;
+         arg.max = RLC_MAX_THPT_TMR; 
+         break;
+      }
       default:
       {
          DU_LOG("\nERROR  -->  RLC : rlcStartTmr: Invalid tmr Evnt [%d]", tmrEvnt);
       }
+      
    } 
 
    if(arg.wait != 0)
@@ -302,6 +311,12 @@ void rlcStopTmr(RlcCb *gCb, PTR cb, uint8_t tmrType)
          arg.max  = RLC_MAX_THPT_TMR;
          break;
       }
+      case EVENT_RLC_DRB_THROUGHPUT_TMR:
+      {
+         arg.timers   = &((RlcThpt *)cb)->drbTputInfo.drbThptTmr;
+         arg.max = RLC_MAX_THPT_TMR; 
+         break;
+      }
       default:
       {
          DU_LOG("\nERROR  -->  RLC : rlcStopTmr: Invalid tmr Evnt[%d]", tmrType);
@@ -391,6 +406,11 @@ Void rlcTmrExpiry(PTR cb,S16 tmrEvnt)
          rlcSnssaiThptTmrExpiry(cb);
          break;
       }
+      case EVENT_RLC_DRB_THROUGHPUT_TMR:
+      {
+         rlcDrbThptTmrExpiry(cb);
+         break;
+      }
       default:
       {
          break;
@@ -451,6 +471,10 @@ bool rlcChkTmr(RlcCb *gCb, PTR cb, int16_t tmrEvnt)
       case EVENT_RLC_SNSSAI_THROUGHPUT_TMR:
       {
          return (((RlcThpt *)cb)->snssaiTputInfo.snssaiThptTmr.tmrEvnt == EVENT_RLC_SNSSAI_THROUGHPUT_TMR);
+      }
+      case EVENT_RLC_DRB_THROUGHPUT_TMR:
+      {
+         return (((RlcThpt *)cb)->drbTputInfo.drbThptTmr.tmrEvnt == EVENT_RLC_DRB_THROUGHPUT_TMR);
       }
       default:
       {
@@ -561,7 +585,7 @@ void rlcUeThptTmrExpiry(PTR cb)
              */
              tpt = (double)(rlcThptCb->ueTputInfo.thptPerUe[ueIdx].dataVol * 8)/(double)ODU_UE_THROUGHPUT_PRINT_TIME_INTERVAL;
       
-             DU_LOG("\nUE Id : %d   DL Tpt : %.2Lf", rlcThptCb->ueTputInfo.thptPerUe[ueIdx].ueId, tpt);
+             DU_LOG("\nUE Id : %d   DL Tpt : %.2Lf (Kbps)", rlcThptCb->ueTputInfo.thptPerUe[ueIdx].ueId, tpt);
              rlcThptCb->ueTputInfo.thptPerUe[ueIdx].dataVol = 0;
          }
       }
@@ -622,6 +646,43 @@ void rlcSnssaiThptTmrExpiry(PTR cb)
    rlcStartTmr(RLC_GET_RLCCB(rlcThptCb->inst), (PTR)rlcThptCb, EVENT_RLC_SNSSAI_THROUGHPUT_TMR);
    return;
 }
+
+/**
+ * @brief Handler to do processing on expiry of the DRB throughput timer
+ *
+ * @details
+ *    This function processes the RLC DRB throughput timer expiry.
+ *
+ * @param[in] cb  Pointer to the RLC throughput struct
+ *
+ * @return  Void
+ */
+void rlcDrbThptTmrExpiry(PTR cb)
+{
+   RlcThpt *rlcThptCb = (RlcThpt*)cb; 
+   uint16_t  ueIdx;
+
+   /* If cell is not up, throughput details cannot be printed */
+   if(gCellStatus != CELL_UP)
+   {
+      /* Restart timer */
+      rlcStartTmr(RLC_GET_RLCCB(rlcThptCb->inst), (PTR)(rlcThptCb), EVENT_RLC_DRB_THROUGHPUT_TMR);
+      return;
+   }
+   
+   for(ueIdx = 0; ueIdx < MAX_NUM_UE; ueIdx++)
+   {
+      if(rlcThptCb->drbTputInfo.dlTputPerDrbList[ueIdx] != NULLP)
+      {
+         rlcCalculateTputPerDrb(rlcThptCb->drbTputInfo.dlTputPerDrbList[ueIdx], DIR_DL);
+      }
+   }
+
+   /* Restart timer */
+   rlcStartTmr(RLC_GET_RLCCB(rlcThptCb->inst), (PTR)rlcThptCb, EVENT_RLC_DRB_THROUGHPUT_TMR);
+   return;
+}
+
 /**
 *
 * @brief filling RLC UE delete configuration
