349ce6153f82d03f3ec90e2450788ea15abe05ed
lal.harshita <Harshita.Lal@radisys.com>
2023-10-05 15:30:37 +0530

    [Epic-ID: ODUHIGH-516][Task-ID: ODUHIGH-531] RIC Subscription Modification Refuse
    Change-Id: Ic69501796d4d90b52d1e717848f6a10a596be2d0
    Signed-off-by: lal.harshita <Harshita.Lal@radisys.com>

--- src/du_app/du_e2ap_msg_hdl.c
+++ src/du_app/du_e2ap_msg_hdl.c
@@ -119,6 +119,62 @@ void fillE2Cause(CauseE2_t *e2Cause, E2FailureCause failureCause)
 
 /*******************************************************************
  *
+ * @brief Printing Type and Cause of failure
+ *
+ * @details
+ *
+ *    Function : printE2ErrorCause
+ *
+ *    Functionality: Printing Type and Cause of failure
+ *
+ * @params[in] E2 Cause
+ * @return void
+ *
+ ******************************************************************/
+void printE2ErrorCause(CauseE2_t *cause)
+{
+   switch(cause->present)
+   {
+      case CauseE2_PR_ricRequest:
+         {
+            DU_LOG("Failure_Type [%s] Cause [%ld]", "RIC_Request", cause->choice.ricRequest);
+            break;
+         }
+      case CauseE2_PR_ricService:
+         {
+            DU_LOG("Failure_Type [%s] Cause [%ld]", "RIC_Service", cause->choice.ricService);
+            break;
+         }
+      case CauseE2_PR_e2Node:
+         {
+            DU_LOG("Failure_Type [%s] Cause [%ld]", "E2_Node", cause->choice.e2Node);
+            break;
+         }
+      case CauseE2_PR_transport:
+         {
+            DU_LOG("Failure_Type [%s] Cause [%ld]", "Transport", cause->choice.transport);
+            break;
+         }
+      case CauseE2_PR_protocol:
+         {
+            DU_LOG("Failure_Type [%s] Cause [%ld]", "Protocol", cause->choice.protocol);
+            break;
+         }
+      case CauseE2_PR_misc:
+         {
+            DU_LOG("Failure_Type [%s] Cause [%ld]", "Miscellaneous", cause->choice.misc);
+            break;
+         }
+      default:
+         {
+            DU_LOG("Failure_Type and Cause unknown");
+            break;
+         }
+   }
+}
+
+/*******************************************************************
+ *
  * @brief Free the ErrorIndication Message
  *
  * @details
@@ -5784,8 +5840,12 @@ uint8_t FillRicSubsModRequired(RICsubscriptionModificationRequired_t *ricSubsMod
    uint8_t ieIdx = 0, elementCnt=0, actionIdx = 0;
    uint8_t numActionsMod = 0, numActionsRmv = 0;
    RICsubscriptionModificationRequired_IEs_t *ricSubsModReqdIe = NULLP;
-   RICactions_RequiredToBeModified_List_t *actionToBeModList = NULLP;
    RICactions_RequiredToBeRemoved_List_t  *actionToBeRmvList = NULLP;
+
+/* Unused in case of E2SM-KPM */
+#if 0
+   RICactions_RequiredToBeModified_List_t *actionToBeModList = NULLP;
+#endif
 
    /* Count number of Actions to be modified or deleted */
    for(actionIdx = 0; actionIdx < MAX_RIC_ACTION; actionIdx++)
@@ -6112,11 +6172,14 @@ void procRicSubscriptionModificationConfirm(E2AP_PDU_t *e2apMsg)
    RICsubscriptionModificationConfirm_t *ricSubsModCfm = NULLP;
    RICsubscriptionModificationConfirm_IEs_t *ricSubsModCfmIe = NULLP;
 
+/* Not used in case of E2SM-KPM */
+#if 0
    RICactions_ConfirmedForModification_List_t *modCfmList = NULLP;
    RICaction_ConfirmedForModification_ItemIEs_t *modCfmListItem = NULLP;
 
    RICactions_RefusedToBeModified_List_t *modRefusedList = NULLP;
    RICaction_RefusedToBeModified_ItemIEs_t *modRefusedListItem = NULLP;
+#endif
 
    RICactions_ConfirmedForRemoval_List_t *rmvCfmList = NULLP;
    RICaction_ConfirmedForRemoval_ItemIEs_t *rmvCfmListItem = NULLP;
@@ -6307,6 +6370,134 @@ void procRicSubscriptionModificationConfirm(E2AP_PDU_t *e2apMsg)
 
 /*******************************************************************
  *
+ * @brief Free APER decoding of RIC Subscription Modification Refuse
+ *
+ * @details
+ *
+ *    Function : freeAperDecodingOfRicSubsModRefuse
+ *
+ * Functionality:  Free APER decoding of RIC Subscription 
+ *   Modification Refuse
+ *
+ * @param  E2AP Message PDU
+ * @return void
+ *
+ ******************************************************************/
+void freeAperDecodingOfRicSubsModRefuse(E2AP_PDU_t *e2apMsg)
+{
+   uint8_t ieIdx =0;
+   RICsubscriptionModificationRefuse_t *ricSubsModRefuse = NULLP;
+
+   if(e2apMsg && e2apMsg->choice.unsuccessfulOutcome)
+   {
+      ricSubsModRefuse = &e2apMsg->choice.unsuccessfulOutcome->value.choice.RICsubscriptionModificationRefuse;
+      if(ricSubsModRefuse->protocolIEs.list.array)
+      {
+         for(ieIdx = 0; ieIdx < ricSubsModRefuse->protocolIEs.list.count; ieIdx++)
+         {
+            if(ricSubsModRefuse->protocolIEs.list.array[ieIdx])
+               free(ricSubsModRefuse->protocolIEs.list.array[ieIdx]);
+         }
+         free(ricSubsModRefuse->protocolIEs.list.array);
+      }
+   }
+}
+
+/*******************************************************************
+ *
+ * @brief Process RIC Subscription Modification Refuse Message
+ *
+ * @details
+ *
+ *    Function : procRicSubscriptionModificationRefuse
+ *
+ * Functionality:  Process RIC Subscription Modification Refuse
+ *    Message received from RIC. 
+ *
+ * @param  E2AP Message PDU
+ * @return void
+ *
+ ******************************************************************/
+void procRicSubscriptionModificationRefuse(E2AP_PDU_t *e2apMsg)
+{
+   uint8_t ieIdx = 0;
+   uint16_t ranFuncId = 0;
+   RicRequestId ricReqId;
+   RICsubscriptionModificationRefuse_t *ricSubsModRefuse = NULLP;
+   RICsubscriptionModificationRefuse_IEs_t *ricSubsModRefuseIe = NULLP;
+   CauseE2_t *cause = NULLP;
+
+   DU_LOG("\nINFO   -->  E2AP : %s: Received RIC Subscription Modification Refuse", __func__);
+
+   do{
+      if(!e2apMsg)
+      {
+         DU_LOG("\nERROR  -->  E2AP : %s: E2AP Message is NULL", __func__);
+         break;
+      }
+
+      if(!e2apMsg->choice.unsuccessfulOutcome)
+      {
+         DU_LOG("\nERROR  -->  E2AP : %s: Unsuccessful Outcome in E2AP message is NULL", __func__);
+         break;
+      }
+
+      ricSubsModRefuse = &e2apMsg->choice.unsuccessfulOutcome->value.choice.RICsubscriptionModificationRefuse;
+      if(!ricSubsModRefuse->protocolIEs.list.array)
+      {
+         DU_LOG("\nERROR  -->  E2AP : %s: Array conatining E2AP message IEs is null", __func__);
+         break;
+      }
+
+      for(ieIdx = 0; ieIdx < ricSubsModRefuse->protocolIEs.list.count; ieIdx++)
+      {
+         if(!ricSubsModRefuse->protocolIEs.list.array[ieIdx])
+         {
+            DU_LOG("\nERROR  -->  E2AP : %s: IE at index [%d] in E2AP message IEs list is null", __func__, ieIdx);
+            break;
+         }
+
+         ricSubsModRefuseIe = ricSubsModRefuse->protocolIEs.list.array[ieIdx];
+         switch(ricSubsModRefuseIe->id)
+         {
+            case ProtocolIE_IDE2_id_RICrequestID:
+               {
+                  memset(&ricReqId, 0, sizeof(RicRequestId));
+                  ricReqId.requestorId = ricSubsModRefuseIe->value.choice.RICrequestID.ricRequestorID;
+                  ricReqId.instanceId = ricSubsModRefuseIe->value.choice.RICrequestID.ricInstanceID;
+                  break;
+               }
+
+            case ProtocolIE_IDE2_id_RANfunctionID:
+               {
+                  ranFuncId = ricSubsModRefuseIe->value.choice.RANfunctionID;
+                  break;
+               }
+
+            case ProtocolIE_IDE2_id_CauseE2:
+               {
+                  DU_LOG("\nDEBUG  -->  E2AP : %s: RIC subscriptiom modification refused for RIC_Requestor_ID [%d] \
+                        RIC_Instance_ID [%d] RAN_Function_ID [%d] ", __func__, ricReqId.requestorId, \
+                        ricReqId.instanceId, ranFuncId);
+
+                  cause = &ricSubsModRefuseIe->value.choice.CauseE2;
+                  printE2ErrorCause(cause);
+               }
+
+            default:
+               break;
+         } /* End of switch for Protocol IE Id */
+      } /* End of for loop for Protocol IE list */
+
+      break;
+   }while(true);
+
+   freeAperDecodingOfRicSubsModRefuse(e2apMsg);
+   return;
+}
+
+/*******************************************************************
+ *
  * @brief Handles received E2AP message and sends back response  
  *
  * @details
@@ -6392,6 +6583,11 @@ void E2APMsgHdlr(Buffer *mBuf)
                case UnsuccessfulOutcomeE2__value_PR_RICserviceUpdateFailure:
                   {
                      procRicServiceUpdateFailure(e2apMsg);
+                     break;
+                  }
+               case UnsuccessfulOutcomeE2__value_PR_RICsubscriptionModificationRefuse:
+                  {
+                     procRicSubscriptionModificationRefuse(e2apMsg);
                      break;
                   }
                default:
