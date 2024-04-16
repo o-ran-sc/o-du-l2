dde9fa28a078245ab601704d6a4129de705fc978
pborla <pborla@radisys.com>
2024-04-16 22:41:22 +0530

    [Epic-ID: ODUHIGH-576][Issue-ID: ODUHIGH-588][SubTask-ID: ODUHIGH-589] | [NFAPI_BRANCH] Fixing the error number issue CmInetSctpConnectx function
    Change-Id: I9c6929ff83941e350dbb5497e948ad2240b2089f
    Signed-off-by: pborla <pborla@radisys.com>
    Signed-off-by: svaidhya <svaidhya@radisys.com>

--- src/5gnrmac/lwr_mac_nfapi.h
+++ src/5gnrmac/lwr_mac_nfapi.h
@@ -1,12 +1,12 @@
  /*******************************************************************************
  ################################################################################
- #   Copyright (c) [20172019] [Radisys]                                        #
+ #   Copyright (c) [2017-2019] [Radisys]                                        #
  #                                                                              #
  #   Licensed under the Apache License, Version 2.0 (the "License");            #
  #   you may not use this file except in compliance with the License.           #
  #   You may obtain a copy of the License at                                    #
  #                                                                              #
- #       http://www.apache.org/licenses/LICENSE2.0                             #
+ #       http://www.apache.org/licenses/LICENSE-2.0                             #
  #                                                                              #
  #   Unless required by applicable law or agreed to in writing, software        #
  #   distributed under the License is distributed on an "AS IS" BASIS,          #
@@ -16,8 +16,8 @@
  ################################################################################
  *******************************************************************************/
 
-#ifndef _LWR_MAC_NFAPI_H_
-#define _LWR_MAC_NFAPI_H_
+ #ifndef _LWR_MAC_NFAPI_H_
+ #define _LWR_MAC_NFAPI_H_
 
 #include "nfapi_interface.h"
 
