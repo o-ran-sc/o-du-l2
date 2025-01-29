ab263447016d94e824ecddbb375f270848d9535b
svaidhya <svaidhya@radisys.com>
2025-01-29 12:33:31 +0000

    Reverted all the changes done on 29Jan2025
    Change-Id: Idfa0aa78efbe3e35c52cb872c1d9556a1bcb221e
    Signed-off-by: svaidhya <svaidhya@radisys.com>

--- setup-com-log-build-deb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "--> setup-com-log-build-deb.sh"
-set -eux -o pipefail
-sudo apt-get update && sudo apt-get -q -y install \
-  autoconf-archive libjsoncpp-dev rpm valgrind
-echo "--> setup-com-log-build-deb.sh ends"
