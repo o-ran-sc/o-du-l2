4bbaa99cdee02600594fa01adb7899b2fab2401a
svaidhya <svaidhya@radisys.com>
2025-06-17 11:35:23 +0000

    MSG2 issue: TBS and TX_Data pdulen matched
    Change-Id: Ie73168500f53f34eec74af5dbcbfbe09f8ad5896
    Signed-off-by: svaidhya <svaidhya@radisys.com>

--- src/5gnrsch/sch_rach.c
+++ src/5gnrsch/sch_rach.c
@@ -913,6 +913,7 @@ uint8_t schFillRar(SchCellCb *cell, SlotTimingInfo rarTime, uint16_t ueId, RarAl
    uint8_t  dmrsStartSymbol, startSymbol, numSymbol ;
    uint16_t numRbs = 0;
    uint16_t tbSize = 0;
+   uint16_t offsetToPointA = 0;
    uint8_t freqDomainResource[FREQ_DOM_RSRC_SIZE] = {0};
    PdschCfg *pdsch;
 
@@ -923,6 +924,7 @@ uint8_t schFillRar(SchCellCb *cell, SlotTimingInfo rarTime, uint16_t ueId, RarAl
       DU_LOG("\nERROR  --> SCH : Memory allocation failed in %s",__func__);
       return RFAILED;
    }
+   offsetToPointA = cell->cellCfg.dlCfgCommon.schFreqInfoDlSib.offsetToPointA;
    PdcchCfg *pdcch = rarAlloc->rarPdcchCfg;
    BwpCfg *bwp = &rarAlloc->bwp;
 
@@ -948,8 +950,8 @@ uint8_t schFillRar(SchCellCb *cell, SlotTimingInfo rarTime, uint16_t ueId, RarAl
    bwp->freqAlloc.startPrb = initialBwp->bwp.freqAlloc.startPrb;
    pdcch->coresetCfg.durationSymbols = numSymbols;
 #else
-   bwp->freqAlloc.numPrb   = 48;
-   bwp->freqAlloc.startPrb = 110;
+   bwp->freqAlloc.numPrb   = numRbs;
+   bwp->freqAlloc.startPrb = ((offsetToPointA >> initialBwp->bwp.scs) - coresetIdxTable[coreset0Idx][3]);
    pdcch->coresetCfg.durationSymbols = 1;
 #endif
    bwp->subcarrierSpacing  = initialBwp->bwp.scs;
@@ -1003,9 +1005,6 @@ uint8_t schFillRar(SchCellCb *cell, SlotTimingInfo rarTime, uint16_t ueId, RarAl
       /* RAR PDU length and FAPI payload header length */
       tbSize = schCalcTbSize(RAR_PAYLOAD_SIZE + TX_PAYLOAD_HDR_LEN);
       pdsch->codeword[cwCount].tbSize = tbSize;
-#ifdef OAI_TESTING
-      pdsch->codeword[cwCount].tbSize = 28;
-#endif
    }
    pdsch->dataScramblingId = cell->cellCfg.phyCellId;
    pdsch->numLayers = 1;
@@ -1030,14 +1029,10 @@ uint8_t schFillRar(SchCellCb *cell, SlotTimingInfo rarTime, uint16_t ueId, RarAl
 
    pdsch->pdschFreqAlloc.vrbPrbMapping = 0; /* non-interleaved */
    pdsch->pdschFreqAlloc.resourceAllocType = 1; /* RAT type-1 RIV format */
-   pdsch->pdschFreqAlloc.startPrb = MAX_NUM_RB;
+   pdsch->pdschFreqAlloc.startPrb = 0;
    pdsch->pdschFreqAlloc.numPrb = \
       schCalcNumPrb(tbSize, mcs, initialBwp->pdschCommon.timeDomRsrcAllocList[k0Index].lengthSymbol);
 
-#ifdef OAI_TESTING
-   pdsch->pdschFreqAlloc.startPrb = 0;
-   pdsch->pdschFreqAlloc.numPrb = 8;
-#endif
    /* Find total symbols occupied including DMRS */
    dmrsStartSymbol = findDmrsStartSymbol(pdsch->dmrs.dlDmrsSymbPos);
    /* If there are no DRMS symbols, findDmrsStartSymbol() returns MAX_SYMB_PER_SLOT, 
